1c833b68e61430f3f0fc7e6ef9c3bf22fb37f3fe
hhw12409 <sksk2425@naver.com>
2022-06-23 12:54:53 +0900

    Feat : add Body component
    add jjal img src

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,10 @@ function Header() {
   return (
     <div>
       <Title>짤 메이커</Title>
+      <form>
+        <input type="text" name="name" placeholder="Write content..."></input>
+        <button type="submit">Create</button>
+      </form>
     </div>
   )
 }
