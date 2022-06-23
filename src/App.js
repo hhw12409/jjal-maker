e38086b7f7d60a1f62eef993557b5d46eb881e25
hhw12409 <sksk2425@naver.com>
2022-06-23 12:54:23 +0900

    Feat : add Body components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Header from './components/Header';
+import Body from './components/Body'
 
 function App() {
+
+  const CAT1 = "https://cataas.com/cat/60b73094e04e18001194a309/says/react";
+
   return (
     <div className="App">
       <Header></Header>
+      <Body img={CAT1}></Body>
     </div>
   );
 }
